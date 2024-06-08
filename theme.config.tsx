92bbd649640c451330c185c7c05c5b6abd190306
chagchagchag <chagchagchag.dev@gmail.com>
2024-06-08 20:05:42 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs GraphQL Study Log</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-graphql-study-log',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-graphql-study-log',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
